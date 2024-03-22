600a1b601a3b99918d50caa5feaca7f509da506b
Arpit Jain <arpt004@gmail.com>
2024-03-23 02:07:50 +0530

    initial commit

---
+++ src/app/page.js
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <h2> Instant Updates </h2>
+    </main>
+  );
+}
