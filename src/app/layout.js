06a3a17d5511ab45a775db9bdd49b7fdeb3cb4ef
Arpit Jain <arpt004@gmail.com>
2024-03-23 02:52:52 +0530

    metadata update

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,7 +5,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Instant Updates",
-  description: "Instant Updates is a next app which provides latest news",
+  description: "Instant Updates is a next app which provides latest news to visitos",
 };
 
 
